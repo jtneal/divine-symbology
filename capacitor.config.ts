00666dd8982d4dc12f33d485c9f823d8ab95b279
Jason Neal <5340961+jtneal@users.noreply.github.com>
2025-07-02 21:25:44 -0400

    Fix lint

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.divinesymbology',
   appName: 'Divine Symbology',
-  webDir: 'dist/apps/web/browser'
+  webDir: 'dist/apps/web/browser',
 };
 
 export default config;
- 
