1854223946ca7e60640eb57b6a49a59c33fcb21f
Jason Neal <5340961+jtneal@users.noreply.github.com>
2025-07-02 21:23:37 -0400

    Adjustments for mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.divinesymbology',
+  appName: 'Divine Symbology',
+  webDir: 'dist/apps/web/browser'
+};
+
+export default config;
+ 
